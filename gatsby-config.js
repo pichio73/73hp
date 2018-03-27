3d4d62e8bdb6beb560c28385134177c43b48ee5d
pichio73 <73hair.pichio@gmail.com>
2018-03-27 12:29:43 +0900

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pathPrefix: `/cat-73hp`,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
+};
