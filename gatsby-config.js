12f1742e0969301507ead421efc092950350f67e
pichio73 <73hair.pichio@gmail.com>
2018-03-27 17:54:46 +0900

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,29 @@
 module.exports = {
   pathPrefix: `/cat-73hp`,
+  siteMetadata: {
+    title: `73hp`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        /*
+        * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
+        * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
+        */
+        baseUrl: `pichio73.work`,
+        // The protocol. This can be http or https.
+        protocol: `http`,
+        // Indicates whether the site is hosted on wordpress.com.
+        // If false, then the asumption is made that the site is self hosted.
+        // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.
+        // If your site is hosted on wordpress.org, then set this to false.
+        hostingWPCOM: false,
+        // If useACF is true, then the source plugin will try to import the Wordpress ACF Plugin contents.
+        // This feature is untested for sites hosted on Wordpress.com
+        useACF: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-typography`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,62 @@
 import React from "react";
 
-export default () => (
-  <div>
-    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
-    <p>
-      What do I like to do? Lots of course but definitely enjoy building
-      websites.
-    </p>
-  </div>
-);
+export default ({ data }) => {
+  return (
+    <div>
+      <h1>{data.site.siteMetadata.title}</h1>
+      <table style={{ tableLayout: `fixed` }}>
+        <thead>
+          <tr>
+            <th>title</th>
+            <th>date</th>
+            <th>guid</th>
+            <th>source_url</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.allWordpressPost.edges.map(({ node }, index) =>
+            <tr key={index}>
+              <td>
+                {node.title}
+              </td>
+              <td>
+                {node.date}
+              </td>
+              <td>
+                {node.guid}
+              </td>
+              <td>
+                {node.featured_media.source_url}
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export const query = graphql`
+  query MyQuery {
+    allWordpressPost{
+      edges{
+        node{
+          title
+          date
+          guid
+          categories {
+            name
+          }
+          featured_media{
+            source_url
+          }
+        }
+      }
+    }    
+   site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
